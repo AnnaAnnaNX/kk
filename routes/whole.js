ec2aa1033257194a2a37212680301d40680d471d
AnnaAnnaNX <anna.annanx@rambler.ru>
2017-11-12 01:38:29 +0400

    all route, input all data

---
+++ routes/whole.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var p = [];
+  p.push({food: 'картошка'});
+
+  res.render('whole', {});
+});
+
+router.post('/', function(req, res, next) {
+  var data = [];
+
+  data.push({x: req.body.food});
+  json = JSON.stringify(data); //convert it back to json
+  fs.writeFileSync('data/whole.json', json, 'utf8');
+
+  res.redirect('/count');
+});
+
+module.exports = router;
